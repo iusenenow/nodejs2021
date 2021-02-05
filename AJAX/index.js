f0ccf28e149013afb889c8968ad3ba1302037053
Wei Zhang <iusenenow@gmail.com>
2021-02-05 22:32:05 +1100

    update

--- AJAX/index.js
+++ AJAX/index.js
@@ -21,8 +21,6 @@ xhr.onload = function () {
   }
 }
 
-
-
 xhr.open('GET', 'sample.txt', true)
 
 console.log('ReadyState: ', xhr.readyState);
@@ -32,3 +30,15 @@ xhr.onerror = function () {
 }
 
 xhr.send()
+
+// readyState values
+// 0: request not initialized
+// 1: server connection established
+// 2: request received
+// 3: processing request
+// 4: request finished and response is ready
+
+// http Statuses
+// 200: "OK"
+// 403: "Forbidden"
+// 404: "Not Found"
